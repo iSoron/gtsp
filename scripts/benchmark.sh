9c3701037f27d658748665e29152db1f8d88d7bc
Alinson Xavier <axavier@uwaterloo.ca>
2015-03-30 17:00:27 -0400

    Accept number of clusters; output to tmp/

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 n=$1
+m=$2
 t=0
 all_t=""
 
 mkdir -p out/
-
 printf "%12s\t%12s\n" "seed" "user-time (s)"
 
 TIMEFORMAT=%U
 for i in $(seq 0 9); do
-    t=$( { time bin/hw2.run --gtsp -s $i -n $n 2>&1 > out/gtsp-n${n}-s${i}.log; } 2>&1 )
+    t=$( { time bin/hw2.run --gtsp -s "$i" -n "$n" -m "$m" 2>&1 > tmp/gtsp-m${m}-n${n}-s${i}.log; } 2>&1 )
     all_t="$all_t $t"
     printf "%12d\t%12.3f\n" $i $t
 done
 
 echo
-echo $all_t | scripts/mean.r
+echo "$all_t" | scripts/mean.r
