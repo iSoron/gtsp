188367e1894bf659d078dc99e4580dd43a062c8e
Alinson Xavier <axavier@uwaterloo.ca>
2015-03-30 11:44:24 -0400

    Create output directory

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,10 +3,13 @@ n=$1
 t=0
 all_t=""
 
-TIMEFORMAT=%U
+mkdir -p out/
+
 printf "%12s\t%12s\n" "seed" "user-time (s)"
+
+TIMEFORMAT=%U
 for i in $(seq 0 9); do
-    t=$( { time bin/hw2.run --gtsp -s $i -n $n > out/gtsp-n${n}-s${i}.log; } 2>&1 )
+    t=$( { time bin/hw2.run --gtsp -s $i -n $n 2>&1 > out/gtsp-n${n}-s${i}.log; } 2>&1 )
     all_t="$all_t $t"
     printf "%12d\t%12.3f\n" $i $t
 done
